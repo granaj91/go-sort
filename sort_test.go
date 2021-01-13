805fab2c10caae76bcdff6f0495d65af24e7b6e4
granaj91 <32178706+granaj91@users.noreply.github.com>
2021-01-13 16:03:56 -0500

    Added web scraper to pull in hackathon events from https://mlh.io/seasons/2021/events

---
+++ sort_test.go
@@ -0,0 +1,103 @@
+package main
+
+import "testing"
+
+func testSmallList(t *testing.T) {
+	testInput := []string{"red", "gold", "black", "maroon"}
+	correctOutput := []string{"black", "gold", "maroon", "red"}
+
+	output := bubbleSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red] , but bubbleSort returned: ", output)
+	}
+
+	output = insertionSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red], but insertionSort returned: ", output)
+	}
+
+	output = selectionSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red] , but selectionSort returned: ", output)
+	}
+
+	output = mergeSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red] , but mergeSort returned: ", output)
+	}
+
+	output = heapSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red], but heapSort returned: ", output)
+	}
+
+}
+
+func testReverseOrderedList(t *testing.T) {
+	testInput := []string{"red", "maroon", "gold", "black"}
+	correctOutput := []string{"black", "gold", "maroon", "red"}
+
+	output := bubbleSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red] , but bubbleSort returned: ", output)
+	}
+
+	output = insertionSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red], but insertionSort returned: ", output)
+	}
+
+	output = selectionSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red] , but selectionSort returned: ", output)
+	}
+
+	output = mergeSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red] , but mergeSort returned: ", output)
+	}
+
+	output = heapSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red], but heapSort returned: ", output)
+	}
+}
+
+func testMixedCaseList(t *testing.T) {
+	testInput := []string{"red", "gold", "Black", "maroon", "Orange"}
+	correctOutput := []string{"Black", "gold", "maroon", "Orange", "red"}
+
+	output := bubbleSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red] , but bubbleSort returned: ", output)
+	}
+
+	output = insertionSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red], but insertionSort returned: ", output)
+	}
+
+	output = selectionSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red] , but selectionSort returned: ", output)
+	}
+
+	output = mergeSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red] , but mergeSort returned: ", output)
+	}
+
+	output = heapSort(testInput)
+	if !isEqual(output, correctOutput) {
+		t.Error("Expected [black gold maroon red], but heapSort returned: ", output)
+	}
+}
+
+func isEqual(a, b []string) bool {
+	for i, value := range a {
+		if value != b[i] {
+			return false
+		}
+	}
+	return true
+}
